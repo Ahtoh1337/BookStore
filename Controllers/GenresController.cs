c7fd696a0075c41f02c633f02defca22251ba5b8
Ahtoh1337 <uvo1anton@gmail.com>
2024-10-04 17:12:23 +0300

    add genres controller

---
+++ Controllers/GenresController.cs
@@ -0,0 +1,36 @@
+using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Controllers;
+
+public class GenresController : Controller
+{
+    private ApplicationContext _dbContext;
+
+    public GenresController(ApplicationContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        var genres = _dbContext.Genres
+            .Include(g => g.Books)
+            .ToDictionary(keySelector: g => g, elementSelector: g => g.Books.Count);
+
+        return View(genres);
+    }
+
+    [HttpGet("[controller]/{id}")]
+    public IActionResult Index(int id)
+    {
+        var genre = _dbContext.Genres.Include(g => g.Books).FirstOrDefault(g => g.GenreId == id);
+
+        if (genre is not null)
+            return View("Genre", genre);
+
+        return NotFound();
+    }
+}
