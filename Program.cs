b9e70f8f2df8a07b5e145e3d1555effc4151d68f
Ahtoh1337 <uvo1anton@gmail.com>
2024-10-16 21:16:48 +0300

    add comments to program.cs

--- Program.cs
+++ Program.cs
@@ -4,11 +4,19 @@ using BookStore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+// Отримання рядка підключення до бази даних
+var connectionString = 
+    builder.Configuration
+    .GetConnectionString("DefaultConnection") ??
+    throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+// Реєстрація сервісу контексту застосунку для роботи з базою даних
 builder.Services.AddDbContext<ApplicationContext>(options =>
     options.UseSqlite(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+// Реєстрація та налаштування ASP.NET Core Identity
 builder.Services.AddDefaultIdentity<IdentityUser>()
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationContext>()
@@ -21,14 +29,16 @@ builder.Services.Configure<IdentityOptions>(o =>
     o.User.RequireUniqueEmail = true;
 });
 
+// Реєстрація сервісів для підтримки патерну Model-View-Controller
 builder.Services.AddRazorPages();
-
 builder.Services.AddControllersWithViews();
 
 
 
 var app = builder.Build();
 
+// Додавання різних middleware, зокрема для аутентифікації
+// та авторизації, підтримки статичних файлів тощо
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -50,8 +60,9 @@ app.MapRazorPages();
 app.MapControllers();
 app.MapDefaultControllerRoute();
 
-
-
+// Додавання сутностей до бази даних при першому запуску програми,
+// зокрема ролі: Admin, Finance, Security, Marketing, User,
+// акаунт адміністратора та декілька книг, авторів та жанрів
 await Seed.SeedRolesAndAdminAsync(app.Services.CreateScope().ServiceProvider);
 Seed.SeedDbContext(app.Services.CreateScope().ServiceProvider.GetRequiredService<ApplicationContext>());
 
